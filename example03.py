dd744aa6a013ebcab6390f8d5fe250d091138392
kadir-zeren <k4dzeren@gmail.com>
2024-07-31 13:35:19 +0300

    logic statements

--- example03.py
+++ example03.py
@@ -30,3 +30,140 @@ r = 5
 pi = 3.14
 area = pi * (r ** 2)
 print (area)
+
+print (11 % 2)
+print((4 * 5) / 2)
+
+print(2 ** 3)
+print(3 ** 2)
+a =2
+b =8
+print((a * b)** 0.5)
+print(5**0.5)
+print(16**(1/2))
+
+a = 5
+a = a + 1
+print(a)
+a +=1
+print(a)
+
+A = 10
+A = A * 2
+A *= 2
+print(A)
+print(3 + 5 * 2)
+
+a = (1 + 1) **(4** (2/2*2) /2)
+print(a)
+a = -3 ** 2
+print(a)
+
+a = (-3) ** 2
+print(a)
+
+a = 3
+b = 4
+hipotenus = (a**2 + b**2)**0.5
+print (hipotenus)
+
+print (a, b, hipotenus)
+print(a,b,hipotenus, sep='-------> ' )
+
+print(a)
+print (b)
+print (hipotenus)
+
+print(a, end="bitiiiiiiiii" )
+print(b)
+print(hipotenus)
+
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+
+print(bir,iki, uc)
+
+print(bir, iki, uc, sep=' ')
+
+print(bir, iki,uc, sep='______bosluk_____')
+
+print (bir)
+print(iki)
+print(uc)
+
+print(bir, end = "\n")
+print(iki, end = '\n')
+print (uc)
+
+print(bir, end = "\n*****")
+print(iki, end = '\n')
+print (uc)
+
+print(bir, iki, uc, sep='* - * ', end = '&' )
+print(bir, iki, uc, sep=' - ')
+print(bir, iki,uc, sep=' - ')
+
+print(bir, iki,uc, sep='* - *',end='&\n')
+print(bir, iki,uc, sep=' ')
+print(bir, iki,uc, sep=' ')
+
+number = 2021
+text = "we have reached"
+print (text, number)
+
+text1 = "I bought"
+text2 = "kg. of apple this morning"
+amount = 6
+text3 = text1 + " " + str(amount) + " " + text2
+print(text1, amount, text2)
+print("I bought", 6, "kg. of apple this morning")
+print ("I bought " + "6 " + "kg. of apple this morning")
+print (text3)
+
+text1 = "I bought"
+text3 = text1 + 'apple'
+print(text3)
+
+print( 'C:|\north pole\noise_penguins.txt')
+print('---------')
+print('first', 'second', 'third', sep='\t')
+
+text = 'Bu\gun \ben \neden\taksiye\bindim'
+print(text)
+
+print( 'Bu\gun\\ben\\neden\\taksiye\\bindim' )
+
+print( 'we are', '\boosting', 'our', '\brotherhood')
+print('it\'s essential to learn Python\'s libraries in IT World')
+print(r'Bu\gun\ben\neden\taksiye\bindim')
+
+print("i'm great")
+print(True and True)
+print(True and False)
+print(False and True and True and True)
+print(True or False or False)
+
+bool_var = True and not True
+print (bool_var)
+
+print(bool(False))
+print(bool (0))
+print(bool([]))
+print(bool(''))
+print(1 and 1)
+print(1 and 0)
+print(bool ('abcdeg' ))
+print(bool (5))
+print(not True)
+print(not False)
+print(not 'string')
+bool_var = True and not True
+print(bool_var)
+print(True and True and True)
+print('string' and 5 and 6.2)
+print(5 and 0 and False and 'abcc')
+print(False or False or False or True)
+print(False or '' or 'False' or '35' or 45)
+print(2 and 3)
+print(1 and 0)
