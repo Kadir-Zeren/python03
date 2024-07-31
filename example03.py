fb2ee5e9a95793a0fd231691998a41e6e3e58b83
kadir-zeren <k4dzeren@gmail.com>
2024-07-31 11:58:54 +0300

    aritmhmetic operations

--- example03.py
+++ example03.py
@@ -5,9 +5,28 @@ print(10 / 2 )
 print(11 // 2)
 print(5 ** 3 )
 print(10 % 3 )
+print(3 +5)
+print("3" + "5")
+print(3 * 5)
+print("abc" * 3)
+print("3" + 'abc')
+print(11-7)
+print(4 + 11.0)
+print('11 - 7')
+print('clarusway') 
+print('3+5')
+num1, num2 = 81, 55
+num3 = num1-num2
+print (num3)
+a,b = 3, 5
+print(a,b)
 
+r = 5
+pi = 3.14
+area = pi * r ** 2
+print(area)
 
-
-
-
-
+r = 5
+pi = 3.14
+area = pi * (r ** 2)
+print (area)
